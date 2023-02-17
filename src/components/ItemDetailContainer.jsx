46595dcefc765307b5214642719333e8f1102e52
sergio lopez <salo.tec.d@gmail.com>
2023-02-17 12:59:44 -0500

    Finalizada entrega 2

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
-// import arrayProductos from "./json/productos.json"
 import arrayJuegos from "./json/games.json"
 const ItemDetailContainer = () => {
 
