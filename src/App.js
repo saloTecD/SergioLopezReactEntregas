8ab45d88f7ab7c94c8d79b6204c2996c8229b038
sergio lopez <salo.tec.d@gmail.com>
2023-02-17 11:06:50 -0500

    inicio preentrega2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import NavBar from "./components/NavBar";
+import Error404 from "./components/Error404";
 
 
 
@@ -15,6 +16,7 @@ function App() {
       <Route path={"/"} element={<ItemListContainer />} />
       <Route path={"/category/:id"} element={<ItemListContainer />} />
       <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+      <Route path={"*"} element={<Error404 />} />
      </Routes>
      
      
