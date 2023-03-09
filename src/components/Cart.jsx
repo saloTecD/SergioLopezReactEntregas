1693e9b423db3057a0798861950babf78a8dd0b4
sergio lopez <salo.tec.d@gmail.com>
2023-03-09 13:43:14 -0500

    proyecto finalizado, se iniciara validacion de email y ruta de id desconocido

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,65 +1,121 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "./context/CartContext";
 import trash from "./images/trash.svg";
+import { addDoc, collection, getFirestore } from "firebase/firestore"
+import { Link} from "react-router-dom";
 
 
 const Cart = () => {
+    const [nombre, setNombre] = useState("")
+    const [telefono, setTelefono] = useState("")
+    const [email, setEmail] = useState("")
+    const [orderId, setOrderId] = useState("")
 
+    const { cart, cartTotal, removeItem, clear, cartSum } = useContext(CartContext)
 
-    const { cart, cartTotal, removeItem, clear, cartSum} = useContext(CartContext)
+    const generarOrden = () => {
+        const buyer = { name: nombre, email: email, phone: telefono }
+        const fecha = new Date()
+        const date = `${fecha.getFullYear()}-${fecha.getMonth() + 1}-${fecha.getDate()}  ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`
+        const order = { buyer: buyer, items: { cart }, date: date, total: cartSum() }
 
-    if (cartTotal() === 0) {
+        const db = getFirestore()
+        const orderCollection = collection(db, "orders")
+        addDoc(orderCollection, order).then(data => {
+            setOrderId(data.id)
+        })
+        checkoutCarrito()
+    }
+
+    const checkoutCarrito=()=>{
+        setTimeout(()=>{
+            clear()
+        },"2000")
+    }
+     if (cartTotal() === 0 && orderId==="") {
+         return (
+             <div className="alert alert-warning text-center" role="alert">
+                 No hay productos en el carrito
+             </div>
+         )
+     }
+
+    if (orderId !== "") {
         return (
-            <div className="alert alert-warning text-center" role="alert">
-                No hay productos en el carrito
+            <div className="container">
+                <div className="alert alert-success text-center" role="alert">
+                    <h3>Gracias por comprar,se ha generado la orden de compra : <b>{orderId}</b></h3>
+                </div>
+                <div className="alert alert-success text-center">
+                   <h5> <Link className="navbar-brand" to="/" onClick={checkoutCarrito}>Haz Click aqui para seguir viendo nuestros productos</Link></h5>
+                </div>
             </div>
         )
     }
 
+
     return (
         <div className="container">
             <div className="row">
+                <div className="col-md-3 my-5">
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="nombre" className="form-label">Nombre</label>
+                            <input type="text" className="form-control" id="nombre" onInput={(e) => { setNombre(e.target.value) }} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="text" className="form-control" id="email" onInput={(e) => { setEmail(e.target.value) }} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="telefono" className="form-label">Telefono</label>
+                            <input type="text" className="form-control" id="telefono" onInput={(e) => { setTelefono(e.target.value) }} />
+                        </div>
+
+                        <button type="button" className="btn btn-primary" onClick={generarOrden}>Generar Compra</button>
+                    </form>
+                </div>
                 <div className="col">
                     <table className="table-color table align-middle">
                         <thead>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td><b>Nombre</b></td>
-                            <td><b>Cantidad</b></td>
-                            <td><b>Precio</b></td>
-                            <td>&nbsp;</td>
-                       </tr>
-                       </thead>
-                       <tbody>
-                        {
-                            cart.map(item=>
-                                <tr key={item.id}>
-                                    <td><img src={item.imagen} alt="" width={300}/></td>
-                                    <td>{item.nombre}</td>
-                                    <td>({item.quantity})</td>
-                                    <td>${item.precio*item.quantity}</td>
-                                    <td><button className="btnVaciarCarrito" onClick={()=>{removeItem(item.id)}}><img src={trash} alt="eliminar producto" /></button></td>
-                                </tr>
-                            )
-                        }
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td><b>Nombre</b></td>
+                                <td><b>Cantidad</b></td>
+                                <td><b>Precio</b></td>
+                                <td>&nbsp;</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                cart.map(item =>
+                                    <tr key={item.id}>
+                                        <td><img src={item.imagen} alt="" width={300} /></td>
+                                        <td>{item.nombre}</td>
+                                        <td>({item.quantity})</td>
+                                        <td>${item.precio * item.quantity}</td>
+                                        <td><button className="btnVaciarCarrito" onClick={() => { removeItem(item.id) }}><img src={trash} alt="eliminar producto" /></button></td>
+                                    </tr>
+                                )
+                            }
                         </tbody>
                         <tfoot>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td>&nbsp;</td>
-                            <td><div className="cellTotal">Total a pagar</div></td>
-                            <td><div className="cellTotal">${cartSum()}</div></td>
-                            <td></td>
-                        </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td><div className="cellTotal">Total a pagar</div></td>
+                                <td><div className="cellTotal">${cartSum()}</div></td>
+                                <td></td>
+                            </tr>
                         </tfoot>
                     </table>
-                    
+
                 </div>
             </div>
             <hr />
             <div className="row">
                 <div className="col d-flex justify-content-end mb-5">
-                    <button className="btnVaciarCarrito" onClick={()=>{clear()}}>Vaciar Carrito</button>
+                    <button className="btnVaciarCarrito" onClick={() => { clear() }}>Vaciar Carrito</button>
                 </div>
             </div>
             
